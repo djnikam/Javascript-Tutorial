ca0f8ad470e57d76bea61bae971ca1f7764a0c22
DHANRAJ JALINDAR NIKAM <dhanrajnikam311@gmail.com>
2024-04-17 22:42:59 +0530

    Rock paper scissors game project completed.

--- DOM/Project/app.js
+++ DOM/Project/app.js
@@ -3,6 +3,11 @@ let rock = document.querySelector("#rock");
 let paper = document.querySelector("#paper");
 let scissors = document.querySelector("#scissors");
 let result = document.querySelector("#result");
+let user_score = document.querySelector("#userscore");
+let comp_score = document.querySelector("#compscore");
+
+let userscore = 0;
+let compscore = 0;
 
 // function to get the rand value in the expected range
 function getRandomInt(max) {
@@ -15,33 +20,73 @@ function getRandomInt(max) {
     2 => scissors
 */
 
+user_score.innerText = 'You : 0';
+comp_score.innerText = 'Comp : 0';
 
-rock.addEventListener("mouseover", ()=>{
+rock.addEventListener("click", ()=>{
     rock.classList.add("choice2");
     let compchoice = getRandomInt(3);
     
-    if(compchoice == 1){
-
-        result.innerText = "You lose!☹️";
-        result.style.backgroundColor = "red";
-        console.log("Lose")
-    }else if(compchoice == 2){
-
-        result.innerText = "You Win!😀";
+    if(compchoice == 2){
+        result.innerText = "You Win!😀 Rock beats scissors!";
         result.style.backgroundColor = "green";
+        userscore++;
+    }else if(compchoice == 1){
+        result.innerText = "You lose!☹️ Paper beats rock!";
+        result.style.backgroundColor = "red";
+        compscore++;
     }else{
         result.innerText = "Draw!😊";
         result.style.backgroundColor = "yellow";
     }
+
+    user_score.innerText = `You : ${userscore}`;
+    comp_score.innerText = `Comp : ${compscore}`;
 });
 
-paper.addEventListener("mouseover", ()=>{
+paper.addEventListener("click", ()=>{
 
     paper.classList.add("choice2");
+
+    let compchoice = getRandomInt(3);
+    
+    if(compchoice == 0){
+        result.innerText = "You Win!😀 Paper beats rock!";
+        result.style.backgroundColor = "green";
+        userscore++;
+    }else if(compchoice == 2){
+        result.innerText = "You lose!☹️ Scissors beats paper!";
+        result.style.backgroundColor = "red";
+        compscore++;
+    }else{
+        result.innerText = "Draw!😊";
+        result.style.backgroundColor = "yellow";
+    }
+
+    user_score.innerText = `You : ${userscore}`;
+    comp_score.innerText = `Comp : ${compscore}`;
 });
 
-scissors.addEventListener("mouseover", ()=>{
+scissors.addEventListener("click", ()=>{
 
     scissors.classList.add("choice2");
+
+    let compchoice = getRandomInt(3);
+    
+    if(compchoice == 1){
+        result.innerText = "You Win!😀 Scissors beats paper!";
+        result.style.backgroundColor = "green";
+        userscore++;
+    }else if(compchoice == 0){
+        result.innerText = "You lose!☹️ Rock beats scissors!";
+        result.style.backgroundColor = "red";
+        compscore++;
+    }else{
+        result.innerText = "Draw!😊";
+        result.style.backgroundColor = "yellow";
+    }
+
+    user_score.innerText = `You : ${userscore}`;
+    comp_score.innerText = `Comp : ${compscore}`;
 });
 
